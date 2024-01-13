b09fd8265c80c482eeead81f1afab05692d9cf8e
yusixian <cosine_yu@qq.com>
2024-01-13 17:57:12 +0800

    feat: add kill notify & Admin chatID

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,4 +1,4 @@
-import { BOT_TOKEN } from '@/constants';
+import { ADMIN_CHAT_ID, BOT_TOKEN } from '@/constants';
 import logger from '@/utils/logger';
 import { Telegraf } from 'telegraf';
 
@@ -14,15 +14,16 @@ bot.help((ctx) => ctx.reply('Send me a command'));
 
 // 其他 bot 命令...
 bot.command('test', (ctx) => ctx.reply('喜欢你！'));
-bot.command('hipster', Telegraf.reply('λ'));
 
 // Enable graceful stop
-process.once('SIGINT', async () => {
-  logger.info('SIGINT');
+process.once('SIGINT', () => {
+  logger.info('[SIGINT] 呜呜呜人家要被杀掉惹...');
+  bot.telegram.sendMessage(ADMIN_CHAT_ID, '[SIGINT] 呜呜呜人家要被杀掉惹...');
   bot.stop('SIGINT');
 });
 process.once('SIGTERM', () => {
-  logger.info('SIGTERM');
+  logger.info('[SIGTERM] 呜呜呜人家要被杀掉惹...');
+  bot.telegram.sendMessage(ADMIN_CHAT_ID, '[SIGTERM] 呜呜呜人家要被杀掉惹...');
   bot.stop('SIGTERM');
 });
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const BOT_TOKEN = process.env.BOT_TOKEN ?? '';
+export const ADMIN_CHAT_ID = process.env.ADMIN_CHAT_ID ?? '';
